9191cdf8ae71b1d15d9d6ea05ddeaf24f2a90c90
Mateusz Wrona <mateuszwrona90@gmail.com>
2025-10-01 23:18:55 +0200

    Add project files.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ConsoleApp1;
+
+public class Person(string name, int age, string email)
+{
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+    public string Email { get; set; } = email;
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var person1 = new Person("name1", 18, "abc@abc.com");
+        var person2 = new Person("", 18, "abc@abc.com");
+        var person3 = new Person("name3", 2, "abc@abc.com");
+        var person4 = new Person("name4", 22, "abcabc.com");
+
+        var validate1 = new PersonValidator();
+        validate1.Validate(person1, out string errorMessage1);
+        Console.WriteLine(errorMessage1);
+
+        var validate2 = new PersonValidator();
+        validate2.Validate(person2, out string errorMessage2);
+        Console.WriteLine(errorMessage2);
+
+        var validate3 = new PersonValidator();
+        validate2.Validate(person2, out string errorMessage3);
+        Console.WriteLine(errorMessage3);
+
+        var validate4 = new PersonValidator();
+        validate4.Validate(person3, out string errorMessage4);
+        Console.WriteLine(errorMessage4);
+    }
+}

bb772f9e015b06535f31d705a04e595f53580a7f
Mateusz Wrona <mateuszwrona90@gmail.com>
2025-10-04 22:45:12 +0200

    Person, validator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,25 +5,26 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var person1 = new Person("name1", 18, "abc@abc.com");
-        var person2 = new Person("", 18, "abc@abc.com");
-        var person3 = new Person("name3", 2, "abc@abc.com");
-        var person4 = new Person("name4", 22, "abcabc.com");
+        var validate = new PersonValidator();
+        var People = new List<Person>();
+        {
+            var person1 = new Person("name1", 18, "abc@abc.com");
+            var person2 = new Person("", 18, "abc@abc.com");
+            var person3 = new Person("name3", 2, "abc@abc.com");
+            var person4 = new Person("name4", 22, "abcabc.com");
+        }
 
-        var validate1 = new PersonValidator();
-        validate1.Validate(person1, out string errorMessage1);
-        Console.WriteLine(errorMessage1);
+        foreach (var person in People)
+        {
+            if(validate.Validate(person, out string errorMessage1))
+            {
+                Console.WriteLine($"{person.Name} ({person.Age} {person.Email}) - VALID");
+            }
+            else
+            {
+                Console.WriteLine($"{person.Name} ({person.Age} {person.Email}) - ERROR");
+            }
 
-        var validate2 = new PersonValidator();
-        validate2.Validate(person2, out string errorMessage2);
-        Console.WriteLine(errorMessage2);
-
-        var validate3 = new PersonValidator();
-        validate2.Validate(person2, out string errorMessage3);
-        Console.WriteLine(errorMessage3);
-
-        var validate4 = new PersonValidator();
-        validate4.Validate(person3, out string errorMessage4);
-        Console.WriteLine(errorMessage4);
+        }
     }
 }

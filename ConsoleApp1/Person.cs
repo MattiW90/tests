942806b7c642a07c62f55b2a00e8d9106ca53c11
Mateusz Wrona <mateuszwrona90@gmail.com>
2025-10-04 22:53:48 +0200

    empty

--- ConsoleApp1/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace ConsoleApp1;
-
-public class Person(string name, int age, string email)
-{
-    public string Name { get; set; } = name;
-    public int Age { get; set; } = age;
-    public string Email { get; set; } = email;
-}
